46f6ac6e1c2e9de616379c4e91a796e9362d4c6c
Alvaro <alvaro.martin.crespo.00@gmail.com>
2023-04-30 10:00:15 +0200

    login register

--- resources/views/session/login.blade.php
+++ resources/views/session/login.blade.php
@@ -1,10 +1,15 @@
-@extends('../plantilla/plantilla')
+<!DOCTYPE html>
+<html lang="en">
 
-@section('title', 'Login Howeb')
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <script src="https://cdn.tailwindcss.com"></script>
+    <title>Login Howeb</title>
+</head>
 
-@section('main')
-
-
+<body>
     <div class="min-h-screen bg-blue-100 flex flex-col justify-center sm:py-12">
         <div class="p-10 xs:p-0 mx-auto md:w-full md:max-w-md">
             <div class="bg-white shadow w-full rounded-lg divide-y divide-gray-200">
@@ -14,14 +19,15 @@
                         @csrf
                         @if (isset($error))
                             <span class="text-red-500 text-xs italic">{{ $error }}</span>
-                        @endif  
+                        @endif
                         <label class="font-semibold text-sm text-gray-600 pb-1 block">Correo electrónico</label>
-                        <input name="email" type="email" class="border rounded-lg px-3 py-2 mt-1 mb-5 text-sm w-full"
+                        <input name="email" type="email"
+                            class="border rounded-lg px-3 py-2 mt-1 mb-5 text-sm w-full"
                             placeholder="Correo electrónico" />
 
                         <label class="font-semibold text-sm text-gray-600 pb-1 block">Contraseña</label>
-                        <input name="password" type="password" class="border rounded-lg px-3 py-2 mt-1 mb-5 text-sm w-full"
-                            placeholder="Contraseña" />
+                        <input name="password" type="password"
+                            class="border rounded-lg px-3 py-2 mt-1 mb-5 text-sm w-full" placeholder="Contraseña" />
 
                         <button type="submit"
                             class="bg-[#243859] transition duration-500 ease-in-out transform hover:scale-105 rounded-lg p-3 text-sm text-white w-full">Iniciar
@@ -39,6 +45,6 @@
             </div>
         </div>
     </div>
+</body>
 
-
-@endsection
+</html>
--- resources/views/session/register.blade.php
+++ resources/views/session/register.blade.php
@@ -1,24 +1,30 @@
-@extends('../plantilla/plantilla')
+<!DOCTYPE html>
+<html lang="en">
 
-@section('title', 'Register Howeb')
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <script src="https://cdn.tailwindcss.com"></script>
+    <title>Register Howeb</title>
+</head>
 
-@section('main')
-
-
-    <div class="min-h-screen bg-blue-100 flex flex-col justify-center sm:py-12">
+<body>
+    <div class="h-screen bg-blue-100 flex flex-col justify-center sm:py-12">
         <div class="p-10 xs:p-0 mx-auto md:w-full md:max-w-md">
-            {{-- <h1 class="font-bold text-center text-3xl mb-5">Iniciar sesión</h1> --}}
             <div class="bg-white shadow w-full rounded-lg divide-y divide-gray-200">
                 <img src="{{ asset('img/landing/icons/logoSinFondo.png') }}" alt="logoSinFondo">
                 <div class="px-5 py-7">
                     <form action="{{ route('register-validate') }}" method="post">
                         @csrf
                         <label class="font-semibold text-sm text-gray-600 pb-1 block">Nombre completo</label>
-                        <input name="name" type="text" class="border rounded-lg px-3 py-2 mt-1 mb-5 text-sm w-full"
+                        <input name="name" type="text"
+                            class="border rounded-lg px-3 py-2 mt-1 mb-5 text-sm w-full"
                             placeholder="Nombre completo" />
 
                         <label class="font-semibold text-sm text-gray-600 pb-1 block">Correo electrónico</label>
-                        <input name="email" type="email" class="border rounded-lg px-3 py-2 mt-1 mb-5 text-sm w-full"
+                        <input name="email" type="email"
+                            class="border rounded-lg px-3 py-2 mt-1 mb-5 text-sm w-full"
                             placeholder="Correo electrónico" />
                         {{-- If the passwords already exists, an error will be thrown. --}}
                         @if (isset($errorEmail))
@@ -28,8 +34,8 @@
                         @endif
 
                         <label class="font-semibold text-sm text-gray-600 pb-1 block">Contraseña</label>
-                        <input name="password" type="password" class="border rounded-lg px-3 py-2 mt-1 mb-5 text-sm w-full"
-                            placeholder="Contraseña" />
+                        <input name="password" type="password"
+                            class="border rounded-lg px-3 py-2 mt-1 mb-5 text-sm w-full" placeholder="Contraseña" />
 
                         <label class="font-semibold text-sm text-gray-600 pb-1 block">Confirmar contraseña</label>
                         <input name="repeatPassword" type="password"
@@ -56,5 +62,6 @@
             </div>
         </div>
     </div>
+</body>
 
-@endsection
+</html>

ed1e99d9c88a7e12a45e4237d1ac334f1f9da9d7
Alvaro <alvaro.martin.crespo.00@gmail.com>
2023-05-04 17:28:47 +0200

    update html

--- resources/views/page/reservation/reservationStep2.blade.php
+++ resources/views/page/reservation/reservationStep2.blade.php
@@ -4,70 +4,74 @@
 
 @section('main')
 
-    <section class="container mx-auto mt-[7rem] p-[2rem]">
+    <div class="bg-[#E5E7EB] p-5">
+        <section class="container mx-auto mt-[7rem] p-[2rem]">
 
-        <div class="flex items-center justify-center">
-            <span class="w-5 h-5 rounded-full bg-green-500 flex justify-center"></span>
-            <span class="border border-green-500 w-16"></span>
-            <span class="w-5 h-5 rounded-full bg-green-500 flex justify-center"></span>
-            <span class="border border-gray-300 w-16"></span>
-            <span class="w-5 h-5 rounded-full bg-gray-300 flex justify-center"></span>
-        </div>
+            <div class="flex items-center justify-center">
+                <span class="w-5 h-5 rounded-full bg-green-500 flex justify-center"></span>
+                <span class="border border-green-500 w-16"></span>
+                <span class="w-5 h-5 rounded-full bg-green-500 flex justify-center"></span>
+                <span class="border border-gray-300 w-16"></span>
+                <span class="w-5 h-5 rounded-full bg-gray-300 flex justify-center"></span>
+            </div>
 
-        <form method="get" action="{{ route('reservationStep3', ['id' => $accomodation->id]) }}">
-            @csrf
+            <form method="get" action="{{ route('reservationStep3', ['id' => $accomodation->id]) }}">
+                @csrf
 
-            @include('page/reservation/DataClient/DataClient')
+                @include('page/reservation/DataClient/DataClient')
 
-            <div class="grid grid-rows-2">
+                <div class="grid grid-row">
 
-                <div class="grid grid-cols-1">
+                    <div class="grid grid-cols-1">
 
-                    <div class="max-w-4xl mx-auto py-12 sm:px-6 lg:px-8">
-                        <h1 class="text-4xl font-extrabold text-center text-orange-600 mb-8">Seleccione Fechas</h1>
+                        <div class="max-w-4xl mx-auto py-12 sm:px-6 lg:px-8">
+                            <h1 class="text-4xl font-extrabold text-center text-orange-600 mb-8">Seleccione Fechas</h1>
 
-                        <div class="mb-10">
-                            <label for="entryDate">
-                                <input name="entryDate" type="date"
-                                    class="bg-gray-100 text-gray-800 border border-gray-400 rounded-lg py-2 px-4">
-                            </label>
+                            <div class="mb-10">
+                                <label for="entryDate">
+                                    <input name="entryDate" type="date"
+                                        class="bg-gray-100 text-gray-800 border border-gray-400 rounded-lg py-2 px-4">
+                                </label>
 
-                            <label for="departureDate">
-                                <input name="departureDate" type="date"
-                                    class="bg-gray-100 text-gray-800 border border-gray-400 rounded-lg py-2 px-4">
-                            </label>
+                                <label for="departureDate">
+                                    <input name="departureDate" type="date"
+                                        class="bg-gray-100 text-gray-800 border border-gray-400 rounded-lg py-2 px-4">
+                                </label>
+                            </div>
+
+
+                            <div class="flex justify-center items-center gap-5">
+                                <a class="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded
+                                focus:outline-none focus:shadow-outline"
+                                    href="{{ route('reservation', ['id' => $accomodation->id]) }}">Volver</a>
+                                <button
+                                    class="bg-orange-500 hover:bg-orange-700 text-white font-bold py-2 px-4 rounded
+                                focus:outline-none focus:shadow-outline"
+                                    type="submit">Enviar</button>
+                            </div>
+
                         </div>
 
-
-                        <div class="flex justify-center items-center gap-5">
-                            <a class="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded
-                                focus:outline-none focus:shadow-outline"
-                                href="{{ route('reservation', ['id' => $accomodation->id]) }}">Volver</a>
-                            <button
-                                class="bg-orange-500 hover:bg-orange-700 text-white font-bold py-2 px-4 rounded
-                                focus:outline-none focus:shadow-outline"
-                                type="submit">Enviar</button>
-                        </div>
+                        @if ($error)
+                            <div class="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative"
+                                role="alert">
+                                <strong class="font-bold">Error:</strong>
+                                <span class="block sm:inline">Las fechas introducidas no son válidas. Por favor, introduzca
+                                    fechas válidas.</span>
+                                <span class="absolute top-0 bottom-0 right-0 px-4 py-3">
+                                </span>
+                            </div>
+                        @endif
 
                     </div>
 
-                    @if ($error)
-                        <div class="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative"
-                            role="alert">
-                            <strong class="font-bold">Error:</strong>
-                            <span class="block sm:inline">Las fechas introducidas no son válidas. Por favor, introduzca
-                                fechas válidas.</span>
-                            <span class="absolute top-0 bottom-0 right-0 px-4 py-3">
-                            </span>
-                        </div>
-                    @endif
-
                 </div>
+            </form>
 
-            </div>
-        </form>
+        </section>
+    </div>
 
-    </section>
+
 
 @endsection
 
